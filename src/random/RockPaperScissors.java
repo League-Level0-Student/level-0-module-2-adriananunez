f6e54915a60c12772954e0e106b5fe0892d30348
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-27 10:42:35 -0700

    RockPaperScissors

--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -79,11 +79,8 @@ public class RockPaperScissors extends JPanel implements ActionListener{
 
         //2. Change the value of opponentSelection to be a random number between 0 and 2;
 	//   Don't forget to create a Random object.
-	    
-        //2. Change the value of opponentSelection to be a random number between 0 and 2; 
-        int opponentSelection = 0;
-        Random randy=new Random();
-		r=randy.nextInt(311)-222;
+	  Random randy=new Random();
+      int opponentSelection=randy.nextInt(3);
         //3. Run the program again. Is the result different?
  
         int selection = 0;
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -6,18 +6,38 @@ package random;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
+import javax.swing.plaf.FontUIResource;
 
 public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
+		for (int i = 0; i <10; i++) {
+			
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+			if(randomNumber==5 ) {
+				JOptionPane.showMessageDialog(null,"You look nice!");
+			}
+			if(randomNumber==1 ) {
+				JOptionPane.showMessageDialog(null,"I like your shoes");
+			}if(randomNumber==2 ) {
+				JOptionPane.showMessageDialog(null,"I like your hat");
+			}
+			if(randomNumber==3 ) {
+				JOptionPane.showMessageDialog(null,"I like your earings");
+			}
+			if(randomNumber==4 ) {
+				JOptionPane.showMessageDialog(null,"I like your shirt");
+			}
+			
+		}
+			
+			
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
